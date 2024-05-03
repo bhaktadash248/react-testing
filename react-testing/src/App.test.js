63f4220621c72d115b4a02db64bc4e561adbb56a
Bhakta <dashlucky248@gmail.com>
2024-05-03 21:28:18 +0530

    added the tobe function

--- react-testing/src/App.test.js
+++ react-testing/src/App.test.js
@@ -8,7 +8,7 @@ Enzyme.configure({ adapter: new EnzymeAdapter })
 test('render without error', () => {
   const wrapper =  shallow(<App/>)
   const appComponent = wrapper.find("[data-test='component-app']")
-  expect(appComponent.length)
+  expect(appComponent.length).toBe(1)
 })
 
 test('render increment button',()=>{
